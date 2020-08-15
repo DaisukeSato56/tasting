37e3a01c828615143946346a94930a41a56c74b5
DaisukeSato56 <workshift56@gmail.com>
2020-08-15 22:16:38 +0900

    initial commit

---
+++ client/layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-app-bar fixed app color="primary" class="white-text">
+      <v-app-bar-nav-icon class="white--text" />
+      <v-toolbar-title text="入力" />
+    </v-app-bar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer>
+      <span>&copy;</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+</script>
